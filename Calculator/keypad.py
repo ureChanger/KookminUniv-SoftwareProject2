b3d84a4d5cc61dcc20c30401335a6cb8e4d78d9b
G-hyeon <56578913+G-hyeon@users.noreply.github.com>
2019-11-11 18:36:30 +0900

    Create keypad.py

---
+++ Calculator/keypad.py
@@ -0,0 +1,27 @@
+numPadList = [
+    '7', '8', '9',
+    '4', '5', '6',
+    '1', '2', '3',
+    '0', '.', '='
+]
+
+operatorList = [
+    '*', '/',
+    '+', '-',
+    '(', ')',
+    'C',
+]
+
+constantList = {
+    'pi':'3.141592',
+    '빛의 이동 속도 (m/s)':'3E+8',
+    '소리의 이동 속도 (m/s)':'340',
+    '태양과의 평균 거리 (km)':'1.5E+8'
+}
+
+functionList = {
+    'factorial (!)':'factorial',
+    '-> binary':'decToBin',
+    'binary -> dec':'binToDec',
+    '-> roman':'toRoman',
+}
